634a4be8b19ed5c0dc638bc730f3f487e5b6adbc
Sammy Phatlane <phatlanems@gmail.com>
2025-12-14 13:45:05 +0200

    CRM CRUD Application Base Project

---
+++ crm-crud/src/test/java/za/co/sammysa/crm_crud/CrmCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.sammysa.crm_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
